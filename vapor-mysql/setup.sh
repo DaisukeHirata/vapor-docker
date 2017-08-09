44f6eae2bdb64a995954de918645778282ad0ee2
Daisuke Hirata <daisukihirata@gmail.com>
2017-08-09 09:52:08 +0900

    first commit

--- vapor-mysql/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-usermod -d /var/lib/mysql/ mysql
-sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
-service mysql start
-mysql < /tmp/setup.sql
